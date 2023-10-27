e2a1d3214bfd6c729d0df6999a67f70044edd18a
polenginlabu <johnpaul.dj21@gmail.com>
2023-10-27 12:34:39 +0800

    add modal and javascript functions

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,35 @@ import '../fonts/OpenSans/OpenSans-MediumItalic.ttf'
 import '../fonts/OpenSans/OpenSans-Regular.ttf'
 import '../fonts/OpenSans/OpenSans-SemiBold.ttf'
 import '../fonts/OpenSans/OpenSans-SemiBoldItalic.ttf'
+
+var figures = document.querySelectorAll(".figure");
+var readMore = document.querySelectorAll(".read-more");
+var closeBox = document.getElementById("closeBox");
+var targetImage = document.getElementById("target-image");
+
+closeBox.addEventListener("click", function() {
+    closeLightbox()
+});
+
+figures.forEach(function(figure) {
+    figure.addEventListener("click", function() {
+        var src = figure.getAttribute("data-src");
+        openLightbox(src);
+    });
+});
+
+readMore.forEach(function(read) {
+    read.addEventListener("click", function(){
+        var title = read.parentNode.querySelector(".title");
+        console.log("Read more: "+ title.textContent);
+    })
+});
+
+function openLightbox(src) {
+    targetImage.setAttribute("src", src);
+    document.getElementById('Lightbox').style.display = 'block';
+};
+
+function closeLightbox() {
+  document.getElementById('Lightbox').style.display = 'none';
+};
